103fcf887b71ed415ec2af5e6b94179d02690742
Lumieremorte <62851054+Lumieremorte@users.noreply.github.com>
2024-08-27 11:15:49 +0200

    UI_v1.7
    ajout d'éléments dans l'UI craft Table, démarrage script timer du texte de départ

--- Assets/UI/Scripts/CraftTablePanel.cs
+++ Assets/UI/Scripts/CraftTablePanel.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class toto : MonoBehaviour
+public class CraftTablePanel : MonoBehaviour
 {
-    int health = 10;
+    [SerializeField] float currentTime = 0f, maxTime= 5.0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,10 @@ public class toto : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
     {
         
     }
