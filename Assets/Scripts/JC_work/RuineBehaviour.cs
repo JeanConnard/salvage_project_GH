d9e9c7762f465369817f1d0a72ec21ac0df517ec
JeanConnard <biere-chips@hotmail.fr>
2024-08-28 15:54:45 +0200

    All Destroy Element

--- Assets/Scripts/JC_work/RuineBehaviour.cs
+++ Assets/Scripts/JC_work/RuineBehaviour.cs
@@ -2,32 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarBehaviour : DestructibleElement_Parent
+public class RuineBehaviour : DestructibleElement_Parent
 {
 
     void Start()
     {
-        health = 5;
+        health = 7;
 
         OnDestruction += Explosion;
     }
 
     void Update()
     {
-        
+
     }
 
     void Explosion()
     {
         gameObject.SetActive(false);
-        
+
         float _random = Random.Range(-0.1f, 0.1f);
         Vector3 _spawnPosition = transform.position + transform.forward * _random + transform.up * 1;
-        
-        Instantiate(fuel, _spawnPosition, transform.rotation);
-        Instantiate(fuel, _spawnPosition, transform.rotation);
-        Instantiate(enginePart, _spawnPosition, transform.rotation);
+
+        Instantiate(sand, _spawnPosition, transform.rotation);
+        Instantiate(sand, _spawnPosition, transform.rotation);
+        Instantiate(sand, _spawnPosition, transform.rotation);
+        Instantiate(sand, _spawnPosition, transform.rotation);
         Instantiate(rope, _spawnPosition, transform.rotation);
+        Instantiate(trash, _spawnPosition, transform.rotation);
         Instantiate(trash, _spawnPosition, transform.rotation);
         Instantiate(trash, _spawnPosition, transform.rotation);
         Instantiate(trash, _spawnPosition, transform.rotation);
