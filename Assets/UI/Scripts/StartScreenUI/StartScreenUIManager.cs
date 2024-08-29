d5ef1bbeaeef12751f787528023f19edb4bcb0ff
Lumieremorte <62851054+Lumieremorte@users.noreply.github.com>
2024-08-29 17:09:29 +0200

    StartScreen v1.0
    affichage des textes (non fini)
    ajout texte : appuyez pour jouer
    alpha et vitesse changés

--- Assets/UI/Scripts/StartScreenUI/StartScreenUIManager.cs
+++ Assets/UI/Scripts/StartScreenUI/StartScreenUIManager.cs
@@ -7,10 +7,16 @@ using UnityEngine.SceneManagement;
 public class StartScreenUIManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI pressAnyKey;
-    [SerializeField] float alphaValue = 0.05f;    
+    [SerializeField] List<TextMeshProUGUI> textToPrint = new List<TextMeshProUGUI>();
+    [SerializeField] TextMeshProUGUI storyText;
+    [SerializeField] TextMeshProUGUI storyPage2;
+    [SerializeField] TextMeshProUGUI storyPage3;
+    TextMeshProUGUI customText;
+    [SerializeField] bool canStart = false;
+    [SerializeField] float alphaValue = 0.05f;
     [SerializeField] float currentTime = 0f;
     [SerializeField] float maxTime = 0.1f;
-    [SerializeField] string sceneName = "MainScene";    
+    [SerializeField] string sceneName = "NewScene";
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +28,9 @@ public class StartScreenUIManager : MonoBehaviour
     {
         UpdateAlphaTimer(ref currentTime, maxTime);
         KeyPressed();
+        if (canStart)
+            UpdateTextAlphaTimer(currentTime, maxTime);
+
     }
     float UpdateAlphaTimer(ref float _current, float _max)
     {
@@ -40,7 +49,6 @@ public class StartScreenUIManager : MonoBehaviour
             ReverseValue();
         }
         pressAnyKey.alpha += alphaValue;
-        Debug.Log(pressAnyKey.alpha.ToString());
     }
     void ReverseValue()
     {
@@ -48,9 +56,53 @@ public class StartScreenUIManager : MonoBehaviour
     }
     void KeyPressed()
     {
-        if(Input.anyKeyDown)
+        if (Input.anyKeyDown)
         {
-            SceneManager.LoadScene(sceneName);            
+            pressAnyKey.gameObject.SetActive(false);
+            canStart = true;
+            storyText.gameObject.SetActive(true);            
+            TextBehaviour(storyText);
         }
+    }
+    void OpenScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    void TextBehaviour(TextMeshProUGUI _text)
+    {
+        customText = _text;
+        //UpdateStoryTextList();
+    }
+    void UpdateStoryTextList()
+    {
+        int _size = textToPrint.Count;
+        for (int i = 0; i < _size; i++)
+        {
+            Debug.Log(i);
+            
+            textToPrint[i].alpha = 0f;
+            if (textToPrint[i].alpha >= 1f)
+            {
+                ReverseValue();
+            }
+            textToPrint[i].alpha += alphaValue;
+            if (textToPrint[i].alpha < 0f)
+            {
+                return;
+            }
+            TextBehaviour(textToPrint[i]);
+            //if (textToPrint[_size + 1] && textToPrint[_size +1].alpha < 0f)
+            //    OpenScene();
+        }
+    }
+    float UpdateTextAlphaTimer(float _current, float _max)
+    {
+        _current += Time.deltaTime;
+        if (_current >= _max)
+        {
+            _current = 0f;
+            UpdateStoryTextList();
+        }
+        return _current;
     }
 }
