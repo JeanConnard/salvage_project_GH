b2a6bf437148facb2c908e75978b3cb81bbab2fc
JeanConnard <biere-chips@hotmail.fr>
2024-08-29 14:21:05 +0200

    Zombie spawner
    Zombie animation
    NavMesh
    Zombi Spawner

--- Assets/Scripts/ZombieSpawner.cs
+++ Assets/Scripts/ZombieSpawner.cs
@@ -1,56 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ZombieSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject zombiePreFab;
-    [SerializeField] float spawnInterval = 360f; //6 minutes (360 secondes)
-    [SerializeField] int numberOfZombiesToSpawn = 20;
-    [SerializeField] float timeSinceStart = 0f;
-    [SerializeField] Transform[] spawnPoints;
-    [SerializeField] bool canStart = false;
+    //Timer
     [SerializeField] StartMessage startMessage;
-    
+    [SerializeField] bool canStart = false;
+
+    [SerializeField] List<GameObject> zombies = new List<GameObject>();
+    [SerializeField] float timerTotal = 360f; //6 minutes (360 secondes)
+    [SerializeField] float currentTime = 0;
+    [SerializeField] public event Action OnTimerEnd = null;
+
 
     void Start()
     {
-        //startMessage = GetComponent<StartMessage>();
+        //zombies = GetComponent<ZombieAI>();
         startMessage.OnMessageRead += SetBool;
     }
 
     void Update()
     {
        if (canStart)
-            ZombieTimer(ref timeSinceStart, spawnInterval);
+           
 
-        timeSinceStart += Time.deltaTime;
-        if (timeSinceStart >= spawnInterval)
+        currentTime += Time.deltaTime;
+        if (currentTime >= timerTotal)
         {
-            SpawnZombies();
+            //OnTimerEnd?.Invoke();
+            //ZombieAI.
+            int _size = zombies.Count;
+            for (int i = 0; i < _size; i++)
+            {
+                zombies[i].SetActive(true);
+            }
         }
     }
-    void SpawnZombies()
-    {
-        for (int i = 0; i < numberOfZombiesToSpawn; i++) 
-        {
-            int spawnIndex = Random.Range(0, spawnPoints.Length);
-            Transform spawnPoint = spawnPoints[spawnIndex];
-            Instantiate(zombiePreFab, spawnPoint.position, spawnPoint.rotation);
-        }
-    }
-    float ZombieTimer(ref float _current, float _max)
-    {
-        _current += Time.deltaTime;
-        if (_current >= _max)
-        {
-            SpawnZombies();
-        }
-        return _current;
-    }
+
     void SetBool()
     {
         canStart = true;
         Debug.Log(canStart);
     }
+   
 }
