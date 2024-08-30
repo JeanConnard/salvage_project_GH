786c7a7b8865b3a14109b033558af6525819eaa5
Lumieremorte <62851054+Lumieremorte@users.noreply.github.com>
2024-08-30 17:33:59 +0200

    Update HotAirBallon.cs

--- Assets/Scripts/HotAirBallon/HotAirBallon.cs
+++ Assets/Scripts/HotAirBallon/HotAirBallon.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class HotAirBallon : MonoBehaviour
 {
-    [SerializeField] PlayerControler playerControler = null;
-    [SerializeField] bool 
+    [SerializeField] PlayerControler playerControler;
+    [SerializeField] bool canFly = false;
+    [SerializeField] float moveSpeed = 1.5f;
+
     //quand on rentre dans le ballon
     //
     //Félicitations!
@@ -20,22 +22,30 @@ public class HotAirBallon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerControler = GetComponent<PlayerControler>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (canFly)
+            Movement();
         
     }
     private void OnTriggerEnter(Collider other)
     {
         playerControler = other.GetComponent<PlayerControler>();
         if (!playerControler) return;
-
+        Invoke("SetCanFly", 3.0f);
     }
     void Movement()
     {
-
+        transform.position += transform.up * moveSpeed * Time.deltaTime;
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
     }
+    void SetCanFly()
+    {
+        canFly = true;
+    }
+
 }
