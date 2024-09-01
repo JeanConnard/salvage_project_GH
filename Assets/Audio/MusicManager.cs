ab3dada5f50f782122fcd9821604ef60700fad50
Lumieremorte <62851054+Lumieremorte@users.noreply.github.com>
2024-09-01 13:00:22 +0200

    MusicManager

--- Assets/Audio/MusicManager.cs
+++ Assets/Audio/MusicManager.cs
@@ -4,18 +4,54 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    [SerializeField] AudioSource loopMusic = null;
+    [SerializeField] AudioSource timeElapsedMusic = null;
+    [SerializeField] AudioSource lostMusic = null;
+    [SerializeField] AudioSource winMusic = null;
+    [SerializeField] bool canStartWinMusic = false;
+    [SerializeField] bool canStartLostMusic = false;
     // Start is called before the first frame update
     void Start()
     {
-        AudioSource loopMusic = GetComponent<AudioSource>();      
-        AudioSource timeElapsedMusic = GetComponent<AudioSource>();
-        //loopMusic.DisableGamepadOutput();
-        //gameObject.GetComponent<AudioSource>().Play(false);
+        loopMusic = GetComponent<AudioSource>();      
+        timeElapsedMusic = GetComponent<AudioSource>();
+        lostMusic = GetComponent<AudioSource>();
+        winMusic = GetComponent<AudioSource>();
+
+        loopMusic.Play();
+        Invoke("PlayMusic", 367);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void PlayTimeElapsedMusic()
+    {
+        loopMusic.Stop();
+        timeElapsedMusic.Play();
+
+    }
+    public void UpdateWinBool(bool _value)
+    {
+        canStartWinMusic = _value;
+    }
+    public void UpdateLostBool(bool _value)
+    {
+        canStartLostMusic= _value;
+    }
+    void OnWin()
+    {
+        loopMusic.Stop();
+        timeElapsedMusic.Stop();
+        winMusic.Play();
+    }
+    void OnLoose()
+    {
+        loopMusic.Stop();
+        timeElapsedMusic.Stop();
+        winMusic.Stop();
+        lostMusic.Play();
     }
 }
